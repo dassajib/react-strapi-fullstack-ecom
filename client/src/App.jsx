ce279f1119606b3803550007efdcf10f39ddff1a
dassajib <sajibdas.sd@gmail.com>
2023-06-11 14:39:44 +0600

    app.css deleted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import {
   RouterProvider,
   Outlet,
 } from "react-router-dom";
-import './App.css'
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
